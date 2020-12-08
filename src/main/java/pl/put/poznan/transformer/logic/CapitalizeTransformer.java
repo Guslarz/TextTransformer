79cd47fdf873e7e8345acf5aa5a7d1e72c6cb5a8
Tomek Jankowiak <tomek.jankowiak01@gmail.com>
2020-12-08 20:52:29 +0100

    Add CapitalizeTransformer

---
+++ src/main/java/pl/put/poznan/transformer/logic/CapitalizeTransformer.java
@@ -0,0 +1,27 @@
+package pl.put.poznan.transformer.logic;
+
+
+public class CapitalizeTransformer extends ActualTransformer {
+
+  public CapitalizeTransformer(TextTransformer previous) {
+    super(previous);
+  }
+
+  @Override
+  protected String apply(String text) {
+    return Capitalize(text);
+  }
+
+  private String Capitalize(String inputText) {
+
+    String result = "";
+    String[] words = inputText.split(" ");
+
+    for(int i = 0; i < words.length; i++){
+      words[i] = words[i].substring(0, 1).toUpperCase() + words[i].substring(1);
+      result = String.join(" ", result, words[i]);
+    }
+
+    return result;
+  }
+}
