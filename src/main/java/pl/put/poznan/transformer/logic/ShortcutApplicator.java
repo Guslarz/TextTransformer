ba4ca5a3285fe374cd0b735dd8532d1aa0040595
Tomek Jankowiak <tomek.jankowiak01@gmail.com>
2020-12-09 23:13:01 +0100

    Support for different letter size

--- src/main/java/pl/put/poznan/transformer/logic/ShortcutApplicator.java
+++ src/main/java/pl/put/poznan/transformer/logic/ShortcutApplicator.java
@@ -20,12 +20,39 @@ public class ShortcutApplicator extends ActualTransformer{
   private String shorten(String text, String key) {
 
     String shortcut = ShortcutsMap.longToShort.get(key);
+    String lowerCase = text.toLowerCase();
 
-    while (text.contains(key)) {
-      text = text.substring(0, text.indexOf(key)) + shortcut + text.substring(text.indexOf(key) + key.length());
+    while (lowerCase.contains(key)) {
+      StringBuilder oneShortcut = new StringBuilder();
+      int index = lowerCase.indexOf(key);
+      String fullForm = text.substring(index, index + key.length());
+
+      if (checkUpperCase(fullForm)) {
+        shortcut = shortcut.toUpperCase();
+      }
+      else if (checkCapitalize(fullForm)) {
+        shortcut = shortcut.substring(0, 1).toUpperCase() + shortcut.substring(1).toLowerCase();
+      }
+      else {
+        shortcut = shortcut.toLowerCase();
+      }
+
+      oneShortcut.append(text, 0, index);
+      oneShortcut.append(shortcut);
+      oneShortcut.append(text, index + key.length(), text.length());
+
+      text = oneShortcut.toString();
+      lowerCase = text.toLowerCase();
     }
 
     return text;
   }
 
+  private Boolean checkCapitalize(String text) {
+    return Character.isUpperCase(text.charAt(0));
+  }
+
+  private Boolean checkUpperCase(String text) {
+    return text.equals(text.toUpperCase());
+  }
 }
