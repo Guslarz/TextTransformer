c9e6a20175b1dc8c4b0754a34f1d4ee7ef6de581
Priescik <thepriescik80@wp.pl>
2020-12-14 23:25:17 +0100

    Zastanawiałem się czy robić uwzględnianie dodatkowych znaków ("że, że" -> "że,") - brałoby dłuższy str, lub wielkości liter, ale narazie wrzucam najprosztą, podstawową wersję. Powiedzcie co myślicie i dodam w razie czego.

--- src/main/java/pl/put/poznan/transformer/logic/TextTransformerFactory.java
+++ src/main/java/pl/put/poznan/transformer/logic/TextTransformerFactory.java
@@ -26,6 +26,8 @@ public class TextTransformerFactory {
         return new ShortcutApplicator(previous);
       case "expandShortcuts":
         return new ShortcutExpander(previous);
+      case "removeDuplicates":
+        return new DuplicateRemover(previous);
       default:
         throw new UndefinedTransformException(transform);
     }
